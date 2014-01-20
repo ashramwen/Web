e131ecdd43fdf2d51ab2969495cffcb17d10eb85
ashramwen <ashramwen@gmail.com>
2014-01-20 11:48:00 +0800

    mabi

--- mabi/advertise/getdata.php
+++ mabi/advertise/getdata.php
@@ -1,5 +1,6 @@
 <?php
-$url = "http://tw.tbook.mabinogi.gamania.com/Game_House/ShopAdvertise/ShopAdvertise.aspx?Name_Server=mabitw4&CharacterId=4503599627372517&Page=1&Row=7&SearchType=&SortType=&SortOption=1&SearchWord=十六夜";
+$url = "http://tw.tbook.mabinogi.gamania.com/Game_House/ShopAdvertise/ShopAdvertise.aspx?Name_Server=mabitw4&CharacterId=4503599627372517&Page=1&Row=7&SearchType=&SortType=1&SortOption=1&SearchWord=十六夜";
+$url = mb_convert_encoding($url, "BIG-5", "UTF-8");
 $content = file_get_contents($url);
 $content = mb_convert_encoding($content, "UTF-8", "BIG-5");
 echo $content;
--- mabi/test2.php
+++ mabi/test2.php
@@ -9,7 +9,8 @@
 		<textarea style="width:800px;height:600px">
 		<?php
 		$url = "http://tw.tbook.mabinogi.gamania.com/Game_House/ShopAdvertise/ShopAdvertise.aspx?Name_Server=mabitw4&CharacterId=4503599627372517&Page=1&Row=7&SearchType=&SortType=1&SortOption=1&SearchWord=十六夜";
-		$url = urlencode ($url);
+		//$url = urlencode ($url);
+		$url = mb_convert_encoding ($url, "BIG-5", "UTF-8");
 		$content = file_get_contents($url);
 		$content = mb_convert_encoding($content, "UTF-8", "BIG-5");
 		$xml = simplexml_load_string($content);
