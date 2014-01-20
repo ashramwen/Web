28088a70a08ae37cc500c5e3ec6ab6135639bdeb
ashramwen <ashramwen@gmail.com>
2014-01-20 12:02:30 +0800

    housing

--- mabi/advertise/advertise.js
+++
@@ -1,4 +0,0 @@
-angular.module('myModule', ['ui.bootstrap']).directive('selectOnClick', function() {	// Linker function	return function(scope, element, attrs) {		element.bind('click', function() {			this.select();		});	};});
-function AdvertiseCtrl($scope) {
-}
-
