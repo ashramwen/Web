cac1631cdf07b29b5be705230d701c5a2a029019
ashramwen <ashramwen@gmail.com>
2014-01-20 12:29:55 +0800

    mabi

--- mabi/housing/housing.js
+++ mabi/housing/housing.js
@@ -1,3 +1,3 @@
 angular.module('myModule', ['ui.bootstrap']).directive('selectOnClick', function() {	// Linker function	return function(scope, element, attrs) {		element.bind('click', function() {			this.select();		});	};});$(function() {	var request = $.ajax({		url : "getdata.php",		type : "POST",		dataType : "text"	});	request.done(function(msg) {		$("#text1").html(msg);	});	request.fail(function(jqXHR, textStatus) {		alert("Request failed: " + textStatus);	});});
-function AdvertiseCtrl($scope) {
+function HousingCtrl($scope) {
 }
